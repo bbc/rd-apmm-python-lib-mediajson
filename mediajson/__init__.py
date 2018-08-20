9b80cc01af481ba7b75294e999dbebe6d267fb39
Sam Nicholson <sam.nicholson@bbc.co.uk>
2018-08-20 17:12:45 +0100

    Only accept timeranges with dots
    Backs out part of 48eba790

--- mediajson/__init__.py
+++ mediajson/__init__.py
@@ -116,10 +116,10 @@ def decode_value(o):
         if re.match(UUID_REGEX,
                     o):
             return uuid.UUID(o)
-        elif re.match(r'^\d+[:\.]\d+$', o):
-            return Timestamp.from_str(o)
-        elif re.match(r'^(\+|-)\d+[:\.]\d+$', o):
-            return TimeOffset.from_str(o)
+        elif re.match(r'^\d+:\d+$', o):
+            return Timestamp.from_tai_sec_nsec(o)
+        elif re.match(r'^(\+|-)\d+:\d+$', o):
+            return TimeOffset.from_sec_nsec(o)
         elif re.match(r'^(\(|\[)?(\d+[:\.]\d+)?_(\d+[:\.]\d+)?(\)|\])?$', o):
             return TimeRange.from_str(o)
         elif o == "()":
