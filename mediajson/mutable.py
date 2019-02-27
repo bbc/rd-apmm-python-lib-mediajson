7aa4eda68343edc169607e2f772ec559c2db9c5e
Philip de Nier <philipn@rd.bbc.co.uk>
2019-02-27 15:41:01 +0000

    reduce code duplication

--- mediajson/mutable.py
+++ mediajson/mutable.py
@@ -14,7 +14,7 @@
 
 """
 This module contains methods and classes to extend json encoding and decoding
-to cover _mutable_ timestamps, uuids, and fractions.
+to cover _immutable_ timestamps, uuids, and fractions.
 
 To make use of it either use the dumps, loads, dump, and load functions in
 place of the versions from the standard json module, or use the classes
@@ -24,15 +24,12 @@ NMOSJSONEncoder and NMOSJSONDecoder as your encoder and decoder classes.
 from __future__ import absolute_import
 from __future__ import print_function
 
-import uuid
-import json
-from json import JSONEncoder, JSONDecoder
-from fractions import Fraction
-from six import string_types
-import re
-
 import mediatimestamp.mutable as mutable
-import mediatimestamp.immutable as immutable
+from json import JSONEncoder, JSONDecoder
+
+from .base import dump, dumps, encode_value
+from .base import _base_load, _base_loads, _base_decode_value
+from .base import NMOSJSONEncoder
 
 __all__ = ["dump", "dumps", "load", "loads",
            "encode_value", "decode_value",
@@ -40,101 +37,16 @@ __all__ = ["dump", "dumps", "load", "loads",
            "NMOSJSONEncoder", "NMOSJSONDecoder"]
 
 
-UUID_REGEX = re.compile(r'[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}')
-
-
-def dump(*args, **kwargs):
-    if 'cls' not in kwargs and len(args) < 7:
-        kwargs['cls'] = NMOSJSONEncoder
-    elif len(args) >= 7 and args[6] is None:
-        args = args[0:6] + [NMOSJSONEncoder] + args[7:]
-    return json.dump(*args, **kwargs)
-
-
-def dumps(*args, **kwargs):
-    if 'cls' not in kwargs and len(args) < 6:
-        kwargs['cls'] = NMOSJSONEncoder
-    elif len(args) >= 6 and args[5] is None:
-        args = args[0:5] + [NMOSJSONEncoder] + args[6:]
-    return json.dumps(*args, **kwargs)
-
-
 def load(*args, **kwargs):
-    if 'cls' not in kwargs and len(args) < 3:
-        kwargs['cls'] = NMOSJSONDecoder
-    elif len(args) >= 3 and args[2] is None:
-        args = args[0:2] + [NMOSJSONDecoder] + args[3:]
-    return json.load(*args, **kwargs)
+    return _base_load(NMOSJSONDecoder, *args, **kwargs)
 
 
 def loads(*args, **kwargs):
-    if 'cls' not in kwargs and len(args) < 3:
-        kwargs['cls'] = NMOSJSONDecoder
-    elif len(args) >= 3 and args[2] is None:
-        args = args[0:2] + [NMOSJSONDecoder] + args[3:]
-    return json.loads(*args, **kwargs)
-
-
-def encode_value(o, return_no_encode=True):
-    if isinstance(o, dict):
-        if not return_no_encode:
-            return None
-        res = {}
-        for key in o:
-            res[key] = encode_value(o[key])
-        return res
-    elif isinstance(o, list):
-        if not return_no_encode:
-            return None
-        return [encode_value(v) for v in o]
-    elif isinstance(o, uuid.UUID):
-        return str(o)
-    elif isinstance(o, mutable.Timestamp) or isinstance(o, immutable.Timestamp):
-        return o.to_tai_sec_nsec()
-    elif isinstance(o, mutable.TimeOffset) or isinstance(o, immutable.TimeOffset):
-        return o.to_sec_nsec()
-    elif isinstance(o, mutable.TimeRange) or isinstance(o, immutable.TimeRange):
-        return o.to_sec_nsec_range()
-    elif isinstance(o, Fraction):
-        return {"numerator": o.numerator,
-                "denominator": o.denominator}
-    else:
-        return o if return_no_encode else None
+    return _base_loads(NMOSJSONDecoder, *args, **kwargs)
 
 
 def decode_value(o):
-    if isinstance(o, dict):
-        if len(o.keys()) == 2 and "numerator" in o and "denominator" in o:
-            return Fraction(o['numerator'], o['denominator'])
-        else:
-            res = {}
-            for key in o:
-                res[key] = decode_value(o[key])
-            return res
-    elif isinstance(o, list):
-        return [decode_value(v) for v in o]
-    elif isinstance(o, string_types):
-        if re.match(UUID_REGEX,
-                    o):
-            return uuid.UUID(o)
-        elif re.match(r'^\d+:\d+$', o):
-            return mutable.Timestamp.from_tai_sec_nsec(o)
-        elif re.match(r'^(\+|-)\d+:\d+$', o):
-            return mutable.TimeOffset.from_sec_nsec(o)
-        elif re.match(r'^(\(|\[)?(\d+:\d+)?_(\d+:\d+)?(\)|\])?$', o):
-            return mutable.TimeRange.from_str(o)
-        elif o == "()":
-            return mutable.TimeRange.never()
-    return o
-
-
-class NMOSJSONEncoder(JSONEncoder):
-    def default(self, o):
-        result = encode_value(o, return_no_encode=False)
-        if result is not None:
-            return result
-        else:
-            return super(NMOSJSONEncoder, self).default(o)
+    return _base_decode_value(o, mutable.Timestamp, mutable.TimeOffset, mutable.TimeRange)
 
 
 class NMOSJSONDecoder(JSONDecoder):
