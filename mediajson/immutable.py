26a3b5207835bb85b2b0420cde65c915dd48fdb5
James Weaver <james.barrett@bbc.co.uk>
2020-02-07 17:05:32 +0000

    Add typechecking to immutable version

--- mediajson/immutable.py
+++ mediajson/immutable.py
@@ -23,10 +23,12 @@ NMOSJSONEncoder and NMOSJSONDecoder as your encoder and decoder classes.
 
 import mediatimestamp.immutable as immutable
 from json import JSONEncoder, JSONDecoder
+from typing import Tuple
 
 from .base import dump, dumps, encode_value
 from .base import _base_load, _base_loads, _base_decode_value
 from .base import NMOSJSONEncoder
+from .typing import JSONSerialisable, MediaJSONSerialisable
 
 __all__ = ["dump", "dumps", "load", "loads",
            "encode_value", "decode_value",
@@ -34,22 +36,22 @@ __all__ = ["dump", "dumps", "load", "loads",
            "NMOSJSONEncoder", "NMOSJSONDecoder"]
 
 
-def load(*args, **kwargs):
+def load(*args, **kwargs) -> None:
     return _base_load(NMOSJSONDecoder, *args, **kwargs)
 
 
-def loads(*args, **kwargs):
+def loads(*args, **kwargs) -> str:
     return _base_loads(NMOSJSONDecoder, *args, **kwargs)
 
 
-def decode_value(o):
+def decode_value(o: JSONSerialisable) -> MediaJSONSerialisable:
     return _base_decode_value(o, immutable.Timestamp, immutable.TimeOffset, immutable.TimeRange)
 
 
 class NMOSJSONDecoder(JSONDecoder):
-    def raw_decode(self, s, *args, **kwargs):
+    def raw_decode(self, s: str, *args, **kwargs) -> Tuple[MediaJSONSerialisable, int]:
+        value: JSONSerialisable
         (value, offset) = super(NMOSJSONDecoder, self).raw_decode(s,
                                                                   *args,
                                                                   **kwargs)
-        value = decode_value(value)
-        return (value, offset)
+        return (decode_value(value), offset)
