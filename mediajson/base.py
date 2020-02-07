9342aac8c92ec893ad3b0e20858f95075fda224d
James Weaver <james.barrett@bbc.co.uk>
2020-02-07 17:05:32 +0000

    Add typechecking to immutable version

--- mediajson/base.py
+++ mediajson/base.py
@@ -23,9 +23,12 @@ NMOSJSONEncoder and NMOSJSONDecoder as your encoder and decoder classes.
 
 import uuid
 import json
-from json import JSONEncoder
+from json import JSONEncoder, JSONDecoder
 from fractions import Fraction
 import re
+
+from typing import Type, Optional, Union, cast
+from .typing import MediaJSONSerialisable, JSONSerialisable
 
 import mediatimestamp.mutable as mutable
 import mediatimestamp.immutable as immutable
@@ -34,50 +37,55 @@ import mediatimestamp.immutable as immutable
 UUID_REGEX = re.compile(r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')
 
 
-def dump(*args, **kwargs):
+def dump(obj: MediaJSONSerialisable, fp, *args, **kwargs) -> None:
+    _args = list(args)
     if 'cls' not in kwargs and len(args) < 7:
         kwargs['cls'] = NMOSJSONEncoder
-    elif len(args) >= 7 and args[6] is None:
-        args = args[0:6] + [NMOSJSONEncoder] + args[7:]
-    return json.dump(*args, **kwargs)
+    elif len(_args) >= 5 and _args[4] is None:
+        _args = _args[0:4] + [NMOSJSONEncoder] + _args[5:]
+    return json.dump(obj, fp, *_args, **kwargs)
 
 
-def dumps(*args, **kwargs):
-    if 'cls' not in kwargs and len(args) < 6:
+def dumps(obj: MediaJSONSerialisable, *args, **kwargs) -> str:
+    _args = list(args)
+    if 'cls' not in kwargs and len(args) < 5:
         kwargs['cls'] = NMOSJSONEncoder
-    elif len(args) >= 6 and args[5] is None:
-        args = args[0:5] + [NMOSJSONEncoder] + args[6:]
-    return json.dumps(*args, **kwargs)
+    elif len(args) >= 5 and args[4] is None:
+        _args = _args[0:4] + [NMOSJSONEncoder] + _args[5:]
+    return json.dumps(obj, *_args, **kwargs)
 
 
-def _base_load(decoder_cls, *args, **kwargs):
+def _base_load(decoder_cls: Type[JSONDecoder], *args, **kwargs) -> None:
+    _args = list(args)
     if 'cls' not in kwargs and len(args) < 3:
         kwargs['cls'] = decoder_cls
     elif len(args) >= 3 and args[2] is None:
-        args = args[0:2] + [decoder_cls] + args[3:]
-    return json.load(*args, **kwargs)
+        _args = _args[0:2] + [decoder_cls] + _args[3:]
+    return json.load(*_args, **kwargs)
 
 
-def _base_loads(decoder_cls, *args, **kwargs):
+def _base_loads(decoder_cls: Type[JSONDecoder], *args, **kwargs) -> str:
+    _args = list(args)
     if 'cls' not in kwargs and len(args) < 3:
         kwargs['cls'] = decoder_cls
     elif len(args) >= 3 and args[2] is None:
-        args = args[0:2] + [decoder_cls] + args[3:]
-    return json.loads(*args, **kwargs)
+        _args = _args[0:2] + [decoder_cls] + _args[3:]
+    return json.loads(*_args, **kwargs)
 
 
-def encode_value(o, return_no_encode=True):
+def encode_value(o: MediaJSONSerialisable,
+                 return_no_encode=True) -> Union[MediaJSONSerialisable, Optional[JSONSerialisable]]:
     if isinstance(o, dict):
         if not return_no_encode:
             return None
         res = {}
         for key in o:
-            res[key] = encode_value(o[key])
-        return res
+            res[key] = encode_value_or_fail(o[key])
+        return cast(JSONSerialisable, res)
     elif isinstance(o, list):
         if not return_no_encode:
             return None
-        return [encode_value(v) for v in o]
+        return cast(JSONSerialisable, [encode_value_or_fail(v) for v in o])
     elif isinstance(o, uuid.UUID):
         return str(o)
     elif isinstance(o, mutable.Timestamp) or isinstance(o, immutable.Timestamp):
@@ -93,7 +101,14 @@ def encode_value(o, return_no_encode=True):
         return o if return_no_encode else None
 
 
-def _base_decode_value(o, timestamp_cls, timeoffset_cls, timerange_cls):
+def encode_value_or_fail(o: MediaJSONSerialisable) -> Optional[JSONSerialisable]:
+    return cast(Optional[JSONSerialisable], encode_value(o, return_no_encode=False))
+
+
+def _base_decode_value(o: JSONSerialisable,
+                       timestamp_cls: Type[immutable.Timestamp],
+                       timeoffset_cls: Type[immutable.TimeOffset],
+                       timerange_cls: Type[immutable.TimeRange]) -> MediaJSONSerialisable:
     if isinstance(o, dict):
         if len(o.keys()) == 2 and "numerator" in o and "denominator" in o:
             return Fraction(o['numerator'], o['denominator'])
@@ -120,8 +135,8 @@ def _base_decode_value(o, timestamp_cls, timeoffset_cls, timerange_cls):
 
 
 class NMOSJSONEncoder(JSONEncoder):
-    def default(self, o):
-        result = encode_value(o, return_no_encode=False)
+    def default(self, o: MediaJSONSerialisable) -> JSONSerialisable:
+        result = cast(Optional[JSONSerialisable], encode_value_or_fail(o))
         if result is not None:
             return result
         else:
